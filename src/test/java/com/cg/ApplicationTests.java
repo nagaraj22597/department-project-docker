f7464cacb33aab90685282040b6d495d6bbe95fd
Nagaraj <nagarajsbudihal@gmail.com>
2023-10-20 14:10:22 +0530

    compose file commit

--- src/test/java/com/cg/ApplicationTests.java
+++ src/test/java/com/cg/ApplicationTests.java
@@ -1,12 +1,8 @@
 package com.cg;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
